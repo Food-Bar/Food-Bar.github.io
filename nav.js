61033a948d634753c89ee07e0eb88d2c14f2c607
Daniel <44211963+blahBlahSomething@users.noreply.github.com>
2021-04-15 00:11:28 -0700

    added responsive menu and some animation

--- nav.js
+++ nav.js
@@ -1,10 +1,29 @@
 const nav = document.getElementById('nav-bar');
 const welcome = document.getElementById('welcome-bubble');
+const burger = document.getElementById('burger');
+const navLink = document.getElementById('nav-links');
+const content = document.getElementById('content');
+const links = document.getElementsByClassName('nav-link');
 
+// for sticky nav bar
 addEventListener('scroll', () => {
-    if(welcome.getBoundingClientRect().bottom < 0){
+    if (welcome.getBoundingClientRect().bottom < -5) {
         nav.classList.add('fixed');
-    }else{
+        nav.style.backgroundColor = '#333';
+        content.style.marginTop = '5vh';
+    } else {
         nav.classList.remove('fixed');
+        nav.style.backgroundColor = 'transparent';
+        content.style.marginTop = 0;
     }
 });
+
+// for the responsive link menu
+burger.addEventListener('click', () => {
+    navLink.classList.toggle('nav-active');
+    for (let i = 0; i < links.length; i++) {
+        if (links[i].style.animation) links[i].style.animation = '';
+        else links[i].style.animation = `link-fade 0.5s ease forwards ${0.2 * i + 0.5}s`;
+    }
+});
+content.addEventListener('click', () => { navLink.classList.remove('nav-active'); });
